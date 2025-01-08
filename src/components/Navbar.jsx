7c0f3794c380ad2aff67527c5f2cf250d0288184
Souvik_Mondal <sm6984767@gmail.com>
2025-01-08 11:34:36 +0530

    some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -83,6 +83,7 @@ const Header = () => {
             <Link to='/profile' onClick={() => {document.getElementById('my-drawer-3').click()}} className='pb-6'><a>Profile</a></Link>
             <Link to='/registered-events' onClick={() => {document.getElementById('my-drawer-3').click()}} className='pb-6'><a>Registered Events</a></Link>
             <Link to='/score-board' onClick={() => {document.getElementById('my-drawer-3').click()}} className='pb-6'><a>ScoreBoard</a></Link>
+            {/* <Link to='/score-board' onClick={() => {document.getElementById('my-drawer-3').click()}} className='pb-6'><a>ScoreBoard</a></Link> */}
           </div>
         </div>
       </div>
@@ -139,6 +140,7 @@ const Header = () => {
         </DropdownMenu>
         <Link to='/grievances' className='text-foreground hover:text-[#8b4513]'>Report</Link>
         <Link to='/registered-events' className='text-foreground hover:text-[#8b4513] '>Registered Events</Link>
+        <Link to='/score-board' className=''><a>ScoreBoard</a></Link>
         </>
       )}
       
